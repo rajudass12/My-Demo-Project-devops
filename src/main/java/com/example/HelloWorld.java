4db2e5c41af0925ede8f762a7e04b735e7caa291
vikki-iam <vignesh.raveendran31@gmail.com>
2025-05-18 17:52:13 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+public class HelloWorld extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/plain");
