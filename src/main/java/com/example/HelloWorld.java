1e128aa80185e9efd7c2e91771513e6513115ef0
vikki-iam <vignesh.raveendran31@gmail.com>
2025-05-18 17:31:18 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,7 +1,8 @@
-package com.example;
-
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello, DevOps from Java!");
+        while (true) {
+            Thread.sleep(10000); // Sleep to keep container alive
+        }
     }
 }
